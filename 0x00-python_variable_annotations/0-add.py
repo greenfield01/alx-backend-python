995bbed10c1c6e505b4e5641310c679e7a2899bf
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-07-08 11:34:49 +0100

    test

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-""" A type-annotation function add """
+"""A type-annotation function add"""
 
 from base64 import b16decode
 
 
 def add(a: float, b: float) -> float:
-    """" Addition with type-annotation """
+    """Addition with type-annotation"""
     return a + b
-    
